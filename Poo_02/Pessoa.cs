8639d5fdedd31f0c817b8f7220186e71bd67b36f
DuduOliiver <eduardoliveira.11@hotmail.com>
2021-04-02 11:42:13 -0300

    Poo_02

---
+++ Poo_02/Pessoa.cs
@@ -0,0 +1,12 @@
+namespace poo_02{
+    
+    public class Pessoa
+    {
+        public string Nome;
+
+        public int Idade;
+
+    }
+}
+
+
---
+++ Poo_02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace poo_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1, p2;
+
+            p1 = new Pessoa();
+
+            p2 = new Pessoa();
+
+            p1.Nome = Console.ReadLine();
+            p1.Idade = int.Parse(Console.ReadLine());
+
+            p2.Nome = Console.ReadLine();
+            p2.Idade = int.Parse(Console.ReadLine());
+
+            if(p1.Idade == p2.Idade)
+            {
+                Console.WriteLine("Idades são iguais");
+            }else if(p1.Idade > p2.Idade)
+            {
+                Console.WriteLine($"Pessoa mais velha: {p1.Nome}");
+            }else{
+                Console.WriteLine($"Pessoa mais velha: {p2.Nome}");
+            }
+        }
+    }
+}
