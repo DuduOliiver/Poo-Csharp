05f43f7e177f0b4d3d29c1ed0d22ff60c5814546
DuduOliiver <eduardoliveira.11@hotmail.com>
2021-04-03 05:40:01 -0300

    Poo_03

---
+++ Poo_03/Funcionario.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace poo_03
+{
+    public class Funcionario
+    {
+        public string Nome;
+
+        public double Salario;
+
+    }
+}
---
+++ Poo_03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace poo_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f1 = new Funcionario();
+            Funcionario f2 = new Funcionario();
+
+            f1.Nome = Console.ReadLine();
+            f1.Salario = double.Parse(Console.ReadLine());
+
+            f2.Nome = Console.ReadLine();
+            f2.Salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Salário médio = {((f1.Salario + f2.Salario) / 2).ToString("F2")}");
+        }
+    }
+}
