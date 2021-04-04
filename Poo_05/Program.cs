639e922a58275852e6c90a8312e768a27004fa02
DuduOliiver <eduardoliveira.11@hotmail.com>
2021-04-04 07:14:52 -0300

    Poo_05

---
+++ Poo_05/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace poo_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.WriteLine("Digite o nome do produto");
+            p.Nome = Console.ReadLine();
+            Console.WriteLine("Digite o preço do produto");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite a quantidade em estoque");
+            p.Quantidade = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Dados do produto: {p}");
+            Console.WriteLine("");
+
+            Console.WriteLine("Digite a quantidade a ser adicionada ao estoque");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            Console.WriteLine($"Dados atualizados {p}");
+            Console.WriteLine("");
+
+            Console.WriteLine("Digite a quantidade a ser removida do estoque");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+            Console.WriteLine($"Dados Atualizados {p}");
+            Console.WriteLine("");
+
+        }
+    }
+}
