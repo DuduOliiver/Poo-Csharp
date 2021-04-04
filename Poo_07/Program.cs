bdb0c098b489db151508cc8bbbf21baba9974b4d
DuduOliiver <eduardoliveira.11@hotmail.com>
2021-04-04 08:45:54 -0300

    Poo_07

---
+++ Poo_07/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace poo_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f1 = new Funcionario();
+
+            Console.WriteLine("Digite o nome do funcionário");
+            f1.Nome = Console.ReadLine();
+            Console.WriteLine("Salário bruto");
+            f1.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Imposto");
+            f1.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("");
+            Console.WriteLine($"Funcionário: {f1}");
+
+            Console.WriteLine("");
+            Console.WriteLine("Aumento do salário em porcentagem");
+            double qte = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            f1.AumentarSalario(qte);
+
+            Console.WriteLine($"Dados Atualizados: {f1}");
+
+        }
+    }
+}
