69e8b4f6f193d18bc399c8c03f66d226d0c1ba4d
DuduOliiver <eduardoliveira.11@hotmail.com>
2021-04-04 07:52:04 -0300

    Poo_06

---
+++ Poo_06/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace poo_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r1 = new Retangulo();
+
+            r1.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            r1.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"AREA = {r1.Area().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"PERÍMETRO = {r1.Perimetro().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"DIAGONAL = {r1.Diagonal().ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
