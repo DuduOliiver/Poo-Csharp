a67a51b74f594a1ccb22fd6dc1f35421eb6ff602
DuduOliiver <eduardoliveira.11@hotmail.com>
2021-04-06 22:28:57 -0300

    Poo_09

---
+++ Poo_09/ConversorDeMoedas.cs
@@ -0,0 +1,10 @@
+
+namespace poo_09
+{
+    public class ConversorDeMoedas
+    {
+         public static double TotalReais(double d, double q){
+            return d * q;
+        }
+    }
+}
---
+++ Poo_09/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace poo_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dolar, quantidade;
+
+            Console.WriteLine("Qual a cotação do dolar?");
+            dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Quantos doláres você vai comprar ?");
+            quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double Calculo = ConversorDeMoedas.TotalReais(dolar, quantidade);
+
+            Console.WriteLine($"{Calculo.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
